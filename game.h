21acd4d72e060f8b2b129b53625359dd6c14616c
Michel Sénégas <travail.scolaire.lyc@gmail.com>
2021-10-23 17:13:44 +0200

    friend void Case...::tomber(Joueur&);

--- game.h
+++ game.h
@@ -15,9 +15,14 @@ public:
     Game(const std::string path_plateau,const std::string path_cartes,const unsigned int nb_joueurs);
     static std::vector<int> tirage_carrieres(const unsigned int nb_joueurs); // Effectue le tirage au sort pour les carrières des joueurs !!! À CHANGER SI L'ON RAJOUTE DES CARRIERES !!!
     static void lire_fichier(std::vector<std::vector<std::string>>& paragraph_list,const std::string path);
-    friend void CaseCashJoueurs::tomber(Joueur&);
-    friend void CaseCashCagnotteJoueurs::tomber(Joueur& J);
     // void play();
     void mouv_joueur(Joueur& J,const int& de); // Appelle la fonction mouv du joueur (utilisée par les cases, cartes)
     void reset(); // Réinitialise la partie
+    friend void CaseCashJoueurs::tomber(Joueur&);
+    friend void CaseCashCagnotteJoueurs::tomber(Joueur&);
+    friend void CaseChance::tomber(Joueur&);
+    friend void CaseTresor::tomber(Joueur&);
+    friend void CaseSalaire::tomber(Joueur&);
+    friend void CaseEnfant::tomber(Joueur&);
+    friend void CaseMarche::tomber(Joueur&);
 };
